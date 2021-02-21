941bc6b1ab9797cf6013dee03b2ba5700160fdbd
pablo122361 <lito23.pjp@gmail.com>
2021-02-21 00:30:50 -0500

    Requirements for Lab1 complete

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func didTapOnFlashCard(_ sender: Any) {
+        questionLabel.isHidden = true
+    }
 }
 
