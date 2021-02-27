9f633ebc02bb0bacab5e899f5ad28417555c17c9
pablo122361 <lito23.pjp@gmail.com>
2021-02-26 23:57:50 -0500

    Completed optional steps Changed appearance of the flashcards & buttons

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -11,16 +11,65 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var cards: UIView!
     
+    
+    @IBOutlet var answer1: UIButton!
+    @IBOutlet var answer2: UIButton!
+    @IBOutlet var answer3: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+        
+        cards.layer.cornerRadius = 20.0
+        questionLabel.layer.cornerRadius = 20.0
+        answerLabel.layer.cornerRadius = 20.0
+        
+        questionLabel.clipsToBounds = true
+        answerLabel.clipsToBounds = true
+                
+        cards.layer.shadowRadius = 13.0
+        cards.layer.shadowOpacity = 0.4
+        
+        answer1.layer.borderWidth = 2.0
+        answer1.layer.borderColor = #colorLiteral(red: 0.3568627451, green: 0.5176470588, blue: 0.6941176471, alpha: 1)
+        answer1.layer.cornerRadius = 10.0
+        
+        answer2.layer.borderWidth = 2.0
+        answer2.layer.borderColor = #colorLiteral(red: 0.3568627451, green: 0.5176470588, blue: 0.6941176471, alpha: 1)
+        answer2.layer.cornerRadius = 10.0
 
+        answer3.layer.borderWidth = 2.0
+        answer3.layer.borderColor = #colorLiteral(red: 0.3568627451, green: 0.5176470588, blue: 0.6941176471, alpha: 1)
+        answer3.layer.cornerRadius = 10.0
+        
+    }
     
-    @IBAction func didTapOnFlashCard(_ sender: Any) {
+ 
+// Part of the required steps, deleted the Tap Gesture. If you want to see it work, attatch the Tap Gesture to the view w the flash cards
+//    @IBAction func tapOnFlashcard(_ sender: Any) {
+//        if questionLabel.isHidden == false {
+//            questionLabel.isHidden = true
+//        }
+//        else{
+//            questionLabel.isHidden = false
+//        }
+//    }
+    
+    
+    @IBAction func didTapAns1(_ sender: Any) {
+        answer1.isHidden = true
+    }
+    
+    @IBAction func didTapAns2(_ sender: Any) {
+        answer1.isHidden = true
+        answer2.isHidden = true
+        answer3.isHidden = true
         questionLabel.isHidden = true
+    }
+    
+    @IBAction func didTapAns3(_ sender: Any) {
+        answer3.isHidden = true
     }
 }
 
