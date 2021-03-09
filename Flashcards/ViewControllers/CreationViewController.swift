5ffc90fa442b7de08e383fe4e58aa0d5e484a908
pablo122361 <lito23.pjp@gmail.com>
2021-03-09 00:24:02 -0500

    added comments to code

--- Flashcards/ViewControllers/CreationViewController.swift
+++ Flashcards/ViewControllers/CreationViewController.swift
@@ -9,15 +9,17 @@ import UIKit
 
 class CreationViewController: UIViewController {
     
+    // connect CreationView to firstScreenView
     var flashcardsController: firstScreenViewController!
+    // IBOutlet for required text fields
     @IBOutlet weak var questionTextField: UITextField!
     @IBOutlet weak var answerTextField: UITextField!
-    
-    var initialQuestion: String?
-    var initialAnswer: String?
-    
+    // IBOutlet for optional text fields
     @IBOutlet weak var extraAnswerOneTextField: UITextField!
     @IBOutlet weak var extraAnswerTwoTextField: UITextField!
+    // if pencilEmoji is pressed then store default strings in textFields
+    var initialQuestion: String?
+    var initialAnswer: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,27 +28,34 @@ class CreationViewController: UIViewController {
         answerTextField.text = initialAnswer
     }
     
+    // cancel button on top left. If pressed will dismiss the page
     @IBAction func didTapOnCancel(_ sender: Any) {
         dismiss(animated: true)
     }
     
+    // done button on top right
     @IBAction func didTapOnDone(_ sender: Any) {
+        // store required textFields inside of variables
         let questionText = questionTextField.text
         let answerText = answerTextField.text
-        
+        // store optional textFields inside of variables
         let extraAnswerOneText = extraAnswerOneTextField.text
         let extraAnswerTwoText = extraAnswerTwoTextField.text
         
+        // if done is pressed, we expect questionTextField and answerTextField to have values. If they dont we will alert them
         let alert = UIAlertController(title: "Missing text", message: "You need to enter both a question and an answer", preferredStyle: UIAlertController.Style.alert)
-        
+        // we want the alert to stay on screen until "Ok" is pressed, without this, the alert will just be dismissed in a second
         let okAction = UIAlertAction(title: "Ok", style: .default)
         
+        // we check if the required TextFields have values
         if questionText == nil || answerText == nil || questionText!.isEmpty || answerText!.isEmpty {
+            // if this dont have values we alert them
             alert.addAction(okAction)
             present(alert, animated: true)
         } else {
+            // if they do have values we update firstScreenView with this function
             flashcardsController.updateFlashcard(question: questionText!, answer: answerText!, extraAnswerOne: extraAnswerOneText!, extraAnswerTwo: extraAnswerTwoText!)
-            
+            // and we dismiss the page 
             dismiss(animated: true)
         }
             
--- Flashcards/ViewControllers/firstScreenViewController.swift
+++ Flashcards/ViewControllers/firstScreenViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class firstScreenViewController: UIViewController {
     
+    // cards is the view. questionLabel and answerLabel are underneath the cards view, kind of like a div and its children
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var answerLabel: UILabel!
     @IBOutlet weak var cards: UIView!
     
-    
+    // show mulitple choice buttons
     @IBOutlet var answer1: UIButton!
     @IBOutlet var answer2: UIButton!
     @IBOutlet var answer3: UIButton!
@@ -21,6 +22,8 @@ class firstScreenViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // style Cards(View), shadow and corners
+        // if you want both rounded corners and shadows must follow specific rules, look back at notes
         cards.layer.cornerRadius = 20.0
         questionLabel.layer.cornerRadius = 20.0
         answerLabel.layer.cornerRadius = 20.0
@@ -31,6 +34,7 @@ class firstScreenViewController: UIViewController {
         cards.layer.shadowRadius = 13.0
         cards.layer.shadowOpacity = 0.4
         
+        // style the buttons
         answer1.layer.borderWidth = 2.0
         answer1.layer.borderColor = #colorLiteral(red: 0.3568627451, green: 0.5176470588, blue: 0.6941176471, alpha: 1)
         answer1.layer.cornerRadius = 10.0
@@ -46,7 +50,7 @@ class firstScreenViewController: UIViewController {
     }
     
  
-// Part of the required steps, deleted the Tap Gesture. If you want to see it work, attatch the Tap Gesture to the view w the flash cards and connect to this IBAction. (un)Click enable under the "brains" in the tapGesture 
+// Part of the required steps. If you want to see it work, attatch the tapGesture to this IBAction. (un)Click enable under the "brains" in the tapGesture
     @IBAction func didTapOnFlashcard(_ sender: Any) {
         if questionLabel.isHidden == false {
             questionLabel.isHidden = true
@@ -56,11 +60,12 @@ class firstScreenViewController: UIViewController {
         }
     }
     
-    
+    // ans1 is incorrect, if tapped will disappear
     @IBAction func didTapAns1(_ sender: Any) {
         answer1.isHidden = true
     }
     
+    // ans2 is correct, if tapped all choices will disapear and show answerLabel
     @IBAction func didTapAns2(_ sender: Any) {
         answer1.isHidden = true
         answer2.isHidden = true
@@ -68,29 +73,33 @@ class firstScreenViewController: UIViewController {
         questionLabel.isHidden = true
     }
     
+    // ans3 is incorrect, if tapped will disappear
     @IBAction func didTapAns3(_ sender: Any) {
         answer3.isHidden = true
     }
     
+    // used in connection with CreationView. If user presses done, a new question and answer will be shown on the flashcards. Extra answers are optional, thats why you have String? and not String
     func updateFlashcard(question: String, answer: String, extraAnswerOne: String?, extraAnswerTwo: String?){
+        // set Label(s) to their respective parameters
         questionLabel.text = question
         answerLabel.text = answer
         
+        // set button(s) to thier respective parameters. answer2 must be the correct answer
         answer1.setTitle(extraAnswerOne, for: .normal)
         answer2.setTitle(answer, for: .normal)
         answer3.setTitle(extraAnswerTwo, for: .normal)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // we know the destination of the segue is the NavigationController
         let navigationController = segue.destination as! UINavigationController
-        
+        // we know the NavigationController onlt contains a CreationViewController
         let creationController = navigationController.topViewController as! CreationViewController
-        
+        // we set the flashCardsController property to self
         creationController.flashcardsController = self
         
-        creationController.initialQuestion = questionLabel.text
-        creationController.initialAnswer = answerLabel.text
-        
+        // when user clicks the emoji pencil, the questionTextField will be the default questionLabel.text, same for answer. If the '+' button then the textFields will just be blank
+        // we named the pencilEmoji segue "EditSegue"
         if segue.identifier == "EditSegue" {
             creationController.initialQuestion = questionLabel.text
             creationController.initialAnswer = answerLabel.text
